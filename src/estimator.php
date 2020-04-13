d6494a3fbaa7d7b5dec92c9d79fe0f3cca77880d
Christelle <christellekalanda@gmail.com>
2020-04-13 11:45:44 +0200

    loop added for array

--- src/estimator.php
+++ src/estimator.php
@@ -17,19 +17,23 @@ function covid19ImpactEstimator($data)
     "severeImpact"=>$severeImpact
   ];*/
   
-  foreach($data  as $d){
-   
-    $dataObject  =  json_decode($d);
-    $impact = new Impact($dataObject);
-    $severeImpact = new SevereImpact($dataObject);
+  foreach($data  as $array){
 
-    $estimatorJson = [
-      "data" =>$dataObject,
-      "impact"=> $impact,
-      "severeImpact"=>$severeImpact
-    ];
-    array_push($jsonArray,json_encode($estimatorJson));
+    foreach ($array as $item){
+      $dataObject  =  json_decode($d);
+      $impact = new Impact($dataObject);
+      $severeImpact = new SevereImpact($dataObject);
   
+      $estimatorJson = [
+        "data" =>$dataObject,
+        "impact"=> $impact,
+        "severeImpact"=>$severeImpact
+      ];
+      array_push($jsonArray,json_encode($estimatorJson));
+    
+    }
+   
+   
   }
   
   return $jsonArray; //*/
