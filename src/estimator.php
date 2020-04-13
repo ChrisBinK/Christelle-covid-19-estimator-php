7e19c229b8038cdd78500eedf17aec049199254c
Christelle <christellekalanda@gmail.com>
2020-04-13 21:59:55 +0200

    estimator code update

--- src/estimator.php
+++ src/estimator.php
@@ -16,35 +16,20 @@ function covid19ImpactEstimator($data)
     $avgDailyIncomeInUSD = $data['region']['avgDailyIncomeInUSD'];
     $avgDailyIncomePopulation =  $data['region']['avgDailyIncomePopulation'];
     
-    //$dataObject  = json_decode($reportedCases, $periodType,$timeToElapse, $totalHospitalBeds,$population, $avgDailyIncomeInUSD,$avgDailyIncomePopulation );
     $impact = new Impact($reportedCases, $periodType,$timeToElapse, $totalHospitalBeds,$population, $avgDailyIncomeInUSD,$avgDailyIncomePopulation );
     $severeImpact = new SevereImpact($reportedCases, $periodType,$timeToElapse, $totalHospitalBeds,$population, $avgDailyIncomeInUSD,$avgDailyIncomePopulation );
 
     $estimatorJson = [
-      "data" =>$dataObject,
+      "data" =>$data,
       "impact"=> $impact,
       "severeImpact"=>$severeImpact
     ];
-
-    return  array_push($jsonArray, json_endcode($estimatorJson)); //*/
+    array_push($jsonArray, json_endcode($estimatorJson));
+    return $jsonArray ; //*/
  
 }
 
 
-/*$data ='{
-  region: {
-  name: "Africa",
-  avgAge: 19.7,
-  avgDailyIncomeInUSD: 5,
-  avgDailyIncomePopulation: 0.71
-  },
-  periodType: "days",
-  timeToElapse: 58,
-  reportedCases: 674,
-  population: 66622705,
-  totalHospitalBeds: 1380614
-  }';
-covid19ImpactEstimator($data);*/
 
 
 
